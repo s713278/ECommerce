e7b224115b8a56e593a59c142a13943cc4169861
SwamyK <swamy.kunta@gmail.com>
2019-01-04 23:29:24 +0530

    Refactored

--- zen-commerce/browse-shop/src/main/java/com/zensar/BnSApplication.java
+++ zen-commerce/browse-shop/src/main/java/com/zensar/BnSApplication.java
@@ -3,8 +3,11 @@ package com.zensar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import com.zensar.browse.service.CategoryService;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com","com.zensar.browse.controller","com.zensar.discovery.controller"})
@@ -13,7 +16,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class BnSApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BnSApplication.class, args);
+    	 ConfigurableApplicationContext context= SpringApplication.run(BnSApplication.class, args);
+    	CategoryService categoryService = context.getBean("ategoryService",CategoryService.class);
     }
     
     @Scheduled(fixedDelay =1000)
--- zen-commerce/browse-shop/src/main/java/com/zensar/browse/service/impl/CategoryServiceImpl.java
+++ zen-commerce/browse-shop/src/main/java/com/zensar/browse/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.zensar.browse.service.impl;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +16,7 @@ import com.zensar.browse.dto.Category;
 import com.zensar.browse.service.CategoryService;
 
 @Service
+@Scope("singleton")
 public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
@@ -19,6 +24,9 @@ public class CategoryServiceImpl implements CategoryService {
 	private int pageNumber =1;
 	private int PAGESIZE =25;
 	
+	public CategoryServiceImpl() {
+	System.out.println(" CategoryServiceImpl >>>> Default constructor loaded");	
+	}
 	@Override
 	public Category getCategory(long id) {
 		return categoryRepository.findById(id).get();
@@ -33,5 +41,15 @@ public class CategoryServiceImpl implements CategoryService {
 	public Page<Category> getAllCategories(Pageable pageable) {
 		PageRequest request = PageRequest.of(pageable.getPageNumber()  - 1, pageable.getPageSize(), Sort.Direction.ASC, "id");
 		return categoryRepository.findAll(request);
+	}
+	
+	@PostConstruct
+	private void doStartUp() {
+		System.out.println("CategoryServiceImpl >>>> After constuctor");
+	}
+	
+	@PreDestroy
+	private void doEnd() {
+		System.out.println("CategoryServiceImpl >>>> After application context close...");
 	}
 }
